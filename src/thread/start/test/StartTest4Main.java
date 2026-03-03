887c25812c96dcc16df2e69cdcff961db62300dd
hyos <hyozzang99@gmail.com>
2026-03-04 02:50:50 +0900

    Runnable과 Thread를 활용한 문제풀이

---
+++ src/thread/start/test/StartTest4Main.java
@@ -0,0 +1,40 @@
+package thread.start.test;
+
+import util.MyLogger;
+
+public class StartTest4Main {
+    public static void main(String[] args) {
+
+        PrintWork a = new PrintWork("A", 1000);
+        PrintWork b = new PrintWork("B", 500);
+
+        Thread threadA = new Thread(a, "Thread-A");
+        Thread threadB = new Thread(b, "Thread-B");
+
+        threadA.start();
+        threadB.start();
+    }
+
+    static class PrintWork implements Runnable {
+
+        private String content;
+        private long sleepMs;
+
+        public PrintWork(String content, long sleepMs) {
+            this.content = content;
+            this.sleepMs = sleepMs;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                MyLogger.log(content);
+                try {
+                    Thread.sleep(sleepMs);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
